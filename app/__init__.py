8fccf29784d7c31f639ded823a130eb34896b440
KaylaB0 <kaylabyfield0@gmail.com>
2025-04-10 14:49:59 -0500

    Created Flask API Route to process form

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import views
+# Initialize database and migration objects
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import views, models
